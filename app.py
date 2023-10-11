35e0b53aa0d5446b882d2c65d110037081e10fec
akshay23504 <akshay23504@gmail.com>
2023-10-11 17:19:34 -0400

    Header, footer, refer, admin page working

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def home():
 def refer():
     return render_template('refer.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
