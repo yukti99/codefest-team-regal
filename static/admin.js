e0a990f4419a64e7aca85544d804bc4af550853c
Yukti Khurana <yuktikhurana99@gmail.com>
2023-10-12 06:24:37 -0400

    working copy

--- static/admin.js
+++ static/admin.js
@@ -1,157 +1,120 @@
 function filter_difficulty() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("difficulty-dropdown");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("client-table");
-    tr = table.getElementsByTagName("tr");
-    if (filter == "ALL"){
-      displayReferrals(referrals);
-      return
-    }
-    for (i = 1; i < tr.length; i++) {
-      // Hide the row initially.
-      tr[i].style.display = "none";
-  
-      td = tr[i].getElementsByTagName("td");
-      for (var j = 0; j < td.length; j++) {
-        cell = tr[i].getElementsByTagName("td")[j];
-        console.log(cell)
-        if (cell) {
-          if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-            break;
-          } 
-        }
-      }
-    }
-    // for (i = 0; i < tr.length; i++) {
-    //   console.log(tr[i])
-    //   td = tr[i].getElementsByTagName("td")[0];
-    //   if (td) {
-    //     console.log(td)
-    //     console.log(filter)
-    //     console.log(td.innerHTML.toUpperCase());
-    //     console.log(td.innerHTML.toUpperCase().indexOf(filter))
-    //     if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-    //       tr[i].style.display = "";
-    //     } else {
-    //       tr[i].style.display = "none";
-    //     }
-    //   }       
-    // }
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("difficulty-dropdown");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("client-table");
+  tr = table.getElementsByTagName("tr");
+  if (filter == "ALL"){
+    displayReferrals(referrals);
+    return
   }
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display =   "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
 
-  function filter_status() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("client-status-dropdown");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("client-table");
-    tr = table.getElementsByTagName("tr");
-    if (filter == "ALL"){
-      displayReferrals(referrals);
-      return
-    }
-    for (i = 1; i < tr.length; i++) {
-      // Hide the row initially.
-      tr[i].style.display = "none";
-  
-      td = tr[i].getElementsByTagName("td");
-      for (var j = 0; j < td.length; j++) {
-        cell = tr[i].getElementsByTagName("td")[j];
-        console.log(cell)
-        if (cell) {
-          if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-            break;
-          } 
-        }
-      }
-    }
-    // for (i = 0; i < tr.length; i++) {
-    //   td = tr[i].getElementsByTagName("td")[0];
-    //   if (td) {
-    //     if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-    //       tr[i].style.display =   "";
-    //     } else {
-    //       tr[i].style.display = "none";
-    //     }
-    //   }       
-    // }
+function filter_status() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("client-status-dropdown");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("client-table");
+  tr = table.getElementsByTagName("tr");
+  if (filter == "ALL"){
+    displayReferrals(referrals);
+    return
   }
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display =   "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
 
 $(document).ready(function() {
-    $("#search-referrals").autocomplete({
-        source: referrals
-    });
-    displayReferrals(referrals);
+  $("#search-referrals").autocomplete({
+      source: referrals
+  });
+  displayReferrals(referrals);
 });  
 
 $(document).ready(function(){
-    $("#search-referrals").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#referral-table tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
+  $("#search-referrals").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#referral-table tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
+  });
 });
 
 function displayReferrals(referrals){
-    $("#referral-table").empty();
-    for (var r in referrals) {
-        createReferralTile(referrals[r]);
-    }   
+  $("#referral-table").empty();
+  for (var r in referrals) {
+      createReferralTile(referrals[r]);
+  }   
 }
 
 function makeid() {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    let length = 8;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  let length = 8;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
 }
 
 function createReferralTile(referral){
-    let main_div = $('<tr id="referral-row">');
-    // for (var r in referral) {
-    //     div = '<td>'+ referral[r] + "</td>";
-    //     main_div.append(div) 
-    // }
-    div = '<td>'+ referral["first_name"] + "</td>";
-    main_div.append(div) 
-    div = '<td>'+ referral["last_name"] + "</td>";
-    main_div.append(div) 
-    div = '<td>'+ referral["date_of_birth"] + "</td>";
-    main_div.append(div) 
-    div = '<td>'+ referral["postal_code"] + "</td>";
-    main_div.append(div)
-    div = '<td>'+ referral["issues"][0]["issue_type"] + "</td>";
-    main_div.append(div)  
-    div = '<td>'+ makeid() + "</td>";
-    main_div.append(div)  
-    div = '<td>'+ referral["client_status"] + "</td>";
-    main_div.append(div)  
-    // div = '<td><button id="admit-btn" type="button" class="btn btn-success">Admit</button></td>'
-    // main_div.append(div)  
+  let main_div = $('<tr id="referral-row">');
+  // for (var r in referral) {
+  //     div = '<td>'+ referral[r] + "</td>";
+  //     main_div.append(div) 
+  // }
+  div = '<td>'+ referral["first_name"] + "</td>";
+  main_div.append(div) 
+  div = '<td>'+ referral["last_name"] + "</td>";
+  main_div.append(div) 
+  div = '<td>'+ referral["date_of_birth"] + "</td>";
+  main_div.append(div) 
+  div = '<td>'+ referral["postal_code"] + "</td>";
+  main_div.append(div)
+  div = '<td>'+ referral["issue_type"] + "</td>";
+  main_div.append(div)  
+  div = '<td>'+ makeid() + "</td>";
+  main_div.append(div)  
+  div = '<td>'+ referral["client_status"] + "</td>";
+  main_div.append(div)  
+  // div = '<td><button id="admit-btn" type="button" class="btn btn-success">Admit</button></td>'
+  // main_div.append(div)  
 
-    // var diff = ""
-    // for (var i in referral["issues"]){
-    //     diff+=referral["issues"][i].issue_desc+", "
-    // }
-    // console.log(diff)
-    // div = '<td>'+ diff + "</td>";
-    // main_div.append(div) 
+  // var diff = ""
+  // for (var i in referral["issues"]){
+  //     diff+=referral["issues"][i].issue_desc+", "
+  // }
+  // console.log(diff)
+  // div = '<td>'+ diff + "</td>";
+  // main_div.append(div) 
 
-    // div = '<td><button id="admit-btn" type="button" class="btn btn-success">Admit</button></td>'
-    // main_div.append(div) 
+  // div = '<td><button id="admit-btn" type="button" class="btn btn-success">Admit</button></td>'
+  // main_div.append(div) 
 
-    $(main_div).click(function(){
-        let pageURL = "/view_referral/"+referral["client_id"];
-        document.location.href = pageURL;
-    });
-    $('#referral-table').append(main_div); 
+  $(main_div).click(function(){
+      let pageURL = "http://127.0.0.1:5000/view_referral/"+referral["client_id"];
+      document.location.href = pageURL;
+  });
+  $('#referral-table').append(main_div); 
 }
--- static/view_referral.js
+++ static/view_referral.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 function displayClientDetails(info){
     console.log("info");
     console.log(info);
-    let main_div = $('<form method="POST" action="/admit_client">');
+    let main_div = $('<form>');
     let row1 = $('<div class="row mb-4">')
     let div1 = $('<div class="col"><div class="form-outline"><label class="form-label">First name: <div id="details">'+info.first_name+'</div></label></div>');
     let div2 = $('<div class="col"><div class="form-outline"><label class="form-label">Last name: <div id="details">'+info.last_name+'</div></label></div>');
@@ -20,17 +20,18 @@ function displayClientDetails(info){
 
     let row3 = $('<div class="row mb-4">')
     div1 = $('<div class="col"><div class="form-outline sm-1"><label class="form-label">Date of Birth<div id="details">'+info.date_of_birth+'</div></label></div></div>')
-    div2 = $('<div class="col"><div class="form-outline sm-1"><label class="form-label">NHS number<div id="details">'+info.nsh_number+'</div></label></div></div>')
+    div2 = $('<div class="col"><div class="form-outline sm-1"><label class="form-label">NHS number<div id="details">'+info.nhs_number+'</div></label></div></div>')
     row3.append(div1);
     row3.append(div2);
 
+    div1 = $('<div class="col"><div class="form-outline sm-1"><label class="form-label">Email<div id="details">'+info.email+'</div></label></div></div>')
     div2 = $('<div class="col"><div class="form-outline sm-1"><label class="form-label">Phone<div id="details">'+info.phone_number+'</div></label></div></div>')
+    row3.append(div1);
     row3.append(div2);
     difficulty_div = $('<label id="difficulty-header" class="form-label">Difficulty(s)</label>')
     
+
     for (var d in info.issues){
-        print("hbdcjkdsncdjn")
-        console.log(info.issues)
         var c = d+1
         var sub_div = $('<div class="col issue-table">')
         div1 = $('<div class="col"><div class="form-outline sm-1"><label class="form-label-small">Issue Type<div id="details">'+info.issues[d].issue_type+'</div></label></div></div>')
@@ -47,26 +48,19 @@ function displayClientDetails(info){
         sub_div.append(div1)
         sub_div.append(div2)
         sub_div.append(div3)
-
         difficulty_div.append(sub_div)
-        
+       
     }
-    
     difficulty_div.append('</label>')
 
+    
 
     main_div.append(row1);
     main_div.append(row2);
     main_div.append(row3);
     $("#difficulty-header").text("Difficulty(s)");
     main_div.append(difficulty_div);
-    btn_div = '<th><button id="admit-btn" type="submit" class="btn btn-success">Admit<input type="text" name="client_id_submit" value="'+info.issues[d].client_id+'" hidden></button></th>'
-    main_div.append(btn_div)
-
-
     $("#client_details-div").append(main_div);
-
-    
 
 }
 
