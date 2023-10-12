03e88b9a91bd598e01bdd73e70ab04bdbe6b627d
Yukti Khurana <yuktikhurana99@gmail.com>
2023-10-12 04:17:52 -0400

    another filtering

--- static/admin.js
+++ static/admin.js
@@ -20,6 +20,28 @@ function filter_difficulty() {
     }
   }
 
+  function filter_status() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("client-status-dropdown");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("client-table");
+    tr = table.getElementsByTagName("tr");
+    if (filter == "ALL"){
+      displayReferrals(referrals);
+      return
+    }
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display =   "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
 $(document).ready(function() {
     $("#search-referrals").autocomplete({
         source: referrals
